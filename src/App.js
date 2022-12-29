e6ae5cc415503921b4ecdafa142ef3367802a5bf
Bimal Ray <72511459+bimal-creatives@users.noreply.github.com>
2022-12-29 12:32:40 +0530

    changed

--- src/App.js
+++ src/App.js
@@ -3,10 +3,15 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { FiSettings } from "react-icons/fi";
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 
+import { Navbar, Footer, Sidebar, ThemeSettings} from './components';
+import { Ecommerce, Orders, Calender, Employee, Stacked, Pyramid, Customer, Kanban, Area, Bar, Pie, Financial, Colorpicker, ColorMapping, Editor, Line} from './pages';
+
+import { useStateContext } from "./contexts/ContextProider";
+
 import "./App.css";
 
 const App = () => {
-  const activeMenu = false;
+  const { activeMenu } = useStateContext();
 
   return (
     <div>
@@ -25,39 +30,39 @@ const App = () => {
           </div>
           {activeMenu ? (
             <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
-              Sidebar
+              <Sidebar/>
             </div>
           ) : (
-            <div className=" w-0 dark:bg-secondary-dark-bg "> Sidebar w-0 </div>
+            <div className=" w-0 dark:bg-secondary-dark-bg "> <Sidebar/> </div>
           )}
           <div className={ `dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72' : 'flex-2'} ` } >
-            <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full" > Navbar </div>
+            <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full" > <Navbar/> </div>
           </div>
 
         {/* Routes */}
         <div>
         <Routes>
         {/* Dashboard */}
-        <Route path='/' element='ECommerce'/>    
-        <Route path='/ecommerce' element='ECommerce'/>    
+        <Route path='/' element={<Ecommerce/>}/>   
+        <Route path='/ecommerce' element={<Ecommerce/>}/>   
         {/* Pages */}
-        <Route path='/orders' element='orders' />    
-        <Route path='/employees' element='Employees' />    
-        <Route path='/customers' element='Customers' />    
+        <Route path='/orders' element={<Orders/>}/>   
+        <Route path='/employees' element={<Employee/>}/>   
+        <Route path='/customers' element={<Customer/>}/>   
         {/* Apps */}
-        <Route path='/kanban' element='Kanban' />    
-        <Route path='/editor' element='Editor' />    
-        <Route path='/calender' element='Calender' />    
-        <Route path='/color-picker' element='Color-picker' />    
+        <Route path='/kanban' element={<Kanban/>}/>   
+        <Route path='/editor' element={<Editor/>}/>   
+        <Route path='/calender' element={<Calender/>}/>   
+        <Route path='/color-picker' element={<Colorpicker/>}/>   
         {/* Charts */}
-        <Route path='/line' element='Line' />    
-        <Route path='/area' element='Area' />    
-        <Route path='/bar' element='Bar' />    
-        <Route path='/pie' element='Pie' />    
-        <Route path='/financial' element='Financial' />    
-        <Route path='/color-mapping' element='ColorMapping' />    
-        <Route path='/pyramid' element='Pyramid' />    
-        <Route path='/stacked' element='Stacked' />    
+        <Route path='/line' element={<Line/>}/>   
+        <Route path='/area' element={<Area/>}/>   
+        <Route path='/bar' element={<Bar/>}/>   
+        <Route path='/pie' element={<Pie/>}/>   
+        <Route path='/financial' element={<Financial/>}/>   
+        <Route path='/color-mapping' element={<ColorMapping/>}/>   
+        <Route path='/pyramid' element={<Pyramid/>}/>   
+        <Route path='/stacked' element={<Stacked/>}/>   
         </Routes>
         </div>
         </div>
---
+++ src/pages/Charts/ColorMapping.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ColorMapping = () => {
+  return (
+    <div>ColorManagement</div>
+  )
+}
+
+export default ColorMapping
